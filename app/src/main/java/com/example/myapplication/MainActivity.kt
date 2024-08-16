3462b6f43b5b081c8e5a9d95098136e09ca32d91
Abd2 <aharchataalim@gmail.com>
2024-08-16 13:01:10 +0100

    this is abd2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                //cdhchjdcdshcdhvdjvdf
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
